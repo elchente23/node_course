8109b1d3004f25036a52cb38d5c14aa5143d6607
Juan Vicente <elchente23@gmail.com>
2018-03-11 21:55:08 -0700

    Se terminó la clase 12

--- Ejemplo01/app.js
+++ Ejemplo01/app.js
@@ -1,15 +1,5 @@
 console.log('Starting app.js');
 
 const fs = require('fs'),
-      os = require('os'),
+      _ = require("lodash"),
       notes = require('./notes.js');
-
-// let user = os.userInfo();
-
-// fs.appendFileSync('greetings.txt', `Hello ${user.username} you're ${notes.age}`);
-
-let res = notes.addNote();
-
-console.log(res);
-console.log(notes.add(5, 6));
-
